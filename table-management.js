6528dfa4882be7c10c561ed65bcef981ba6cdf73
Tom <tomek.okarma@gmail.com>
2018-05-16 21:04:40 +0200

    modifying table-management code

--- table-management.js
+++ table-management.js
@@ -1,21 +1,21 @@
+  var tableId = 0;
+  var tables = [];
 
-  
-  function addTable(){
-	$(".restaurant").append("<div class='draggable' class='ui-widget-content'></div>")
-	$(".draggable").draggable();
-	$(".draggable").on( "dragstop", function( event, ui ) { console.log(ui.position);} );
-	$(".draggable").draggable({
-		drag: function( event, ui ) {
-		//console.log(ui.position);
-		}
-	});
+  function addTable(){	
+	tableId++;
+	var table = {id:tableId, left:0, top:0}
+	tables.push(table);
+	$(".restaurant").append("<div id='"+tableId+"' class='draggable' class='ui-widget-content'></div>");
+	$(".draggable").draggable();	
+    $(".draggable").on( "dragstop", function( event, ui) { 					
+		saveTablePosition(Number($(this).attr("id")), ui.position.left, ui.position.top);
+	});	
   }
   
-  function createLogInfo(){
-
+  function saveTablePosition(id, left, top) {
+		console.log(tables.findIndex(t => t.id === id))
+		var tableIndex = tables.findIndex(t => t.id === id);
+		tables[tableIndex].left = left;
+		tables[tableIndex].top = top;
   }
-  
-$( ".draggable" ).draggable({
-  drag: function( event, ui ) {}
-});
-
+ 
